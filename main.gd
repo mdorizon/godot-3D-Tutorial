b4494526c6e0dfa0be5976a8130b6c8bab88f86a
MDorizon <mdorizon@proton.me>
2024-06-11 10:40:48 +0200

    :sparkles: retrying the game

--- main.gd
+++ main.gd
@@ -2,6 +2,8 @@ extends Node
 
 @export var mob_scene: PackedScene
 
+func _ready():
+	$UserInterface/Retry.hide()
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
@@ -24,3 +26,9 @@ func _on_mob_timer_timeout():
 
 func _on_player_hit():
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# This restarts the current scene.
+		get_tree().reload_current_scene()
