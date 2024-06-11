c8598ef690dd9b9ccaab0c4309923ae2e937e078
MDorizon <mdorizon@proton.me>
2024-06-11 09:53:43 +0200

    :sparkles: code checkpoint

--- player.gd
+++ player.gd
@@ -1,33 +1,40 @@
 extends CharacterBody3D
 
-# How fast the player moves in meters per second.
+signal hit
+
+# How fast the player moves in meters per second
 @export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
+# The downward acceleration while in the air, in meters per second squared.
 @export var fall_acceleration = 75
 # Vertical impulse applied to the character upon jumping in meters per second.
 @export var jump_impulse = 20
-# Vertical impulse applied to the character upon bouncing over a mob in
-# meters per second.
+# Vertical impulse applied to the character upon bouncing over a mob
+# in meters per second.
 @export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
 
 func _physics_process(delta):
+	# We create a local variable to store the input direction
 	var direction = Vector3.ZERO
 
+	# We check for each move input and update the direction accordingly
 	if Input.is_action_pressed("move_right"):
-		direction.x += 1
+		direction.x = direction.x + 1
 	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
+		direction.x = direction.x - 1
 	if Input.is_action_pressed("move_back"):
-		direction.z += 1
+		# Notice how we are working with the vector's x and z axes.
+		# In 3D, the XZ plane is the ground plane.
+		direction.z = direction.z + 1
 	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
+		direction.z = direction.z - 1
 
+	# Prevent diagonal moving fast af
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-		$Pivot.basis = Basis.looking_at(direction)
+		$Pivot.look_at(position + direction, Vector3.UP)
 
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
@@ -37,15 +44,12 @@ func _physics_process(delta):
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
-	# Moving the Character
-	velocity = target_velocity
-	move_and_slide()
-	
 	# Jumping.
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		target_velocity.y = jump_impulse
-		
-		# Iterate through all collisions that occurred this frame
+
+	# Iterate through all collisions that occurred this frame
+	# in C this would be for(int i = 0; i < collisions.Count; i++)
 	for index in range(get_slide_collision_count()):
 		# We get one of the collisions with the player
 		var collision = get_slide_collision(index)
@@ -64,17 +68,15 @@ func _physics_process(delta):
 				target_velocity.y = bounce_impulse
 				# Prevent further duplicate calls.
 				break
-				
-# Emitted when the player was hit by a mob.
-# Put this at the top of the script.
-signal hit
 
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
 
 # And this function at the bottom.
 func die():
 	hit.emit()
 	queue_free()
-
 
 func _on_mob_detector_body_entered(body):
 	die()
