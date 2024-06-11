b4fbb84135894f95a211bf9c7bfec645213b78f9
MDorizon <mdorizon@proton.me>
2024-06-11 09:29:28 +0200

    :sparkles: squashing monsters

--- Mob.gd
+++ Mob.gd
@@ -1,10 +1,16 @@
 extends CharacterBody3D
 
+# Emitted when the player jumped on the mob.
+signal squashed
+
 # Minimum speed of the mob in meters per second.
 @export var min_speed = 10
 # Maximum speed of the mob in meters per second.
 @export var max_speed = 18
 
+func squash():
+	squashed.emit()
+	queue_free()
 
 func _physics_process(_delta):
 	move_and_slide()
